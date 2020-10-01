423f110a645b5f90341a624fc5a160131e0ab138
Jon D <dyercode@users.noreply.github.com>
2020-10-01 18:04:15 -0400

    show the basket of potatoes. carrots are the anti-potato

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 import { BehaviorSubject, combineLatest, interval, Subject } from "rxjs";
+import { take } from "rxjs/operators";
 
 let timer$ = interval(1000);
 let step$: Subject<number> = new BehaviorSubject(0);
+let potatoes$ = new BehaviorSubject(0);
 
 timer$.subscribe(() => console.log("tick"));
 
 function getPotatoes(): HTMLSpanElement {
   return document.getElementById("potatoes") as HTMLSpanElement;
 }
+
+function getBasket(): HTMLParagraphElement {
+  return document.getElementById("basket") as HTMLParagraphElement;
+}
+
+potatoes$.subscribe((p) => {
+  getPotatoes().innerText = p.toString();
+});
+
+potatoes$.subscribe((p) => {
+  if (p > 0) {
+    getBasket().innerText = "🥔".repeat(p);
+  } else {
+    getBasket().innerText = "🥕".repeat(Math.abs(p));
+  }
+});
 
 (document.getElementById("potato-butt") as HTMLInputElement).addEventListener(
   "change",
@@ -19,7 +37,8 @@ function getPotatoes(): HTMLSpanElement {
   }
 );
 
-combineLatest([step$, timer$]).subscribe(([step, _tick]) => {
-  let currentPotatos: number = parseInt(getPotatoes().innerText) || 0;
-  getPotatoes().innerText = (currentPotatos + step).toString();
+timer$.subscribe(() => {
+  combineLatest([potatoes$, step$])
+    .pipe(take(1))
+    .subscribe(([curr, step]) => potatoes$.next(curr + step));
 });
